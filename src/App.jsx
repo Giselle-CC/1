82e143f641756a52f780ec1301f721c3256709e3
Giselle <giselle.ceballoscardenas@sinaptic.com.ar>
2023-05-31 21:26:55 -0300

    primera parte del parcial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+
+
+function App() {
+  /*const [alumno, setAlumno] = useState({
+    nombre: '',
+    nota: 0
+  })*/
+
+  return (
+    <>
+    <div className='App'>
+
+      <h1>Carga de Estudiantes</h1>
+
+      <form>  </form>
+      
+      <Card/>
+        
+
+    </div>
+ 
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = ({ soyLaProp}) => {
+    return (
+      <>
+        <div>Hola, soy una tarjeta</div>
+        <p> {soyLaProp} </p>
+      </>
+    )
+  }
+
+export default Card;
